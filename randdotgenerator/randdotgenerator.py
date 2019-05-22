63b57b29397da5d4e96d80703cfea551455c2baf
mmernagh <mmernagh@users.noreply.github.com>
2019-05-22 09:59:35 -0700

    Add some rough ideas about libraries

--- randdotgenerator/randdotgenerator.py
+++ randdotgenerator/randdotgenerator.py
@@ -1,1 +1,5 @@
-
+# Suggestions (feel free to ignore)
+# Generating a random image: use numpy to create a square [m, m, 3] RGB color image
+# You can set the quadrant lines by setting pixels to a specific color e.g., [m/2, :, :] = 0, for example
+# You might not even need an image processing library. If you'd really like to read about them though, skimage (scikit) and opencv are good starting points
+# matplotlib will be easy to use to plot the numpy [m, m, 3] array
