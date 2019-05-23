eca09ea8c88f2cde3d645ea3eeb1abb1aea5fabb
Ben McClowsky <bigbenshuffleboard@gmail.com>
2019-05-22 22:10:28 -0400

    Test

--- randdotgenerator/randdotgenerator.py
+++ randdotgenerator/randdotgenerator.py
@@ -1,1 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Create random points
+N = 3000
+x = np.random.rand(N)
+y = np.random.rand(N)
+area = 18
+
+# Plot
+plt.scatter(x, y, s=area, marker='s', edgecolors='none')
+plt.show()
 
