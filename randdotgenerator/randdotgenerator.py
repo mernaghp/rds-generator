fabaf4f5e26ec55c71cda64cae8fd7eaa8878e28
Ben McClowsky <bigbenshuffleboard@gmail.com>
2019-06-11 23:14:01 -0400

    implement random quadrant choice

--- randdotgenerator/randdotgenerator.py
+++ randdotgenerator/randdotgenerator.py
@@ -8,14 +8,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors
 
-x1 = np.random.randint(2, size=(100, 100))
+x1 = np.random.randint(2, size=(500, 500))
 x2 = np.copy(x1)
 
-# will need a loop to randomly choose one of these
-x2[10:30,40:60] = 0 #top
-x2[40:60,70:90] = 0 #right
-x2[70:90,40:60] = 0 #bottom
-x2[40:60,10:30] = 0 #left
+numrows = len(x1)
+numcols = len(x1[0])
+
+quadrant = np.random.randint(1,5)
+if quadrant == 1:
+    x2[100:200,200:300] = 0
+elif quadrant == 2:
+    x2[200:300,100:200] = 0
+elif quadrant == 3:
+    x2[300:400,200:300] = 0
+else:
+    x2[200:300,300:400] = 0
 
 # make a color map of fixed colors - need to make green more "green"
 cmap1 = colors.ListedColormap(['white', 'red'])
@@ -23,10 +30,10 @@ cmap2 = colors.ListedColormap(['white', 'green'])
 
 fig, axs = plt.subplots(1, 2)
 
-im1 = axs[0].imshow(x1, cmap=cmap1)
+im1 = axs[0].imshow(x1, cmap=cmap1, origin='lower')
 axs[0].axis('off')
 
-im2 = axs[1].imshow(x2, cmap=cmap2)
+im2 = axs[1].imshow(x2, cmap=cmap2, origin='lower')
 axs[1].axis('off')
  
 plt.subplots_adjust(left=0.02, bottom=0.02, right=0.98, top=0.98, wspace=0.05)
