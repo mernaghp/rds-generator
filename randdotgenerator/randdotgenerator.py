6448e6e02a484d883dd3fafc4db051fcc6a81b38
Ben McClowsky <bigbenshuffleboard@gmail.com>
2019-07-06 21:29:34 -0400

    change quad to list

--- randdotgenerator/randdotgenerator.py
+++ randdotgenerator/randdotgenerator.py
@@ -11,7 +11,7 @@ from matplotlib import colors
 #numrows = len(x1)  # you can also use x1.shape, which returns a list I believe
 #numcols = len(x1[0])
 
-quad = {1:'bottom', 2:'left', 3:'top', 4:'right'}  # you could also use a list e.g., quad = ['bottom', 'left' ...], and then call quad[i + 1]
+quad = ['bottom', 'left', 'top', 'right']
 
 cmap1 = colors.ListedColormap(['white', 'red'])
 cmap2 = colors.ListedColormap(['white', 'cyan'])
@@ -25,12 +25,12 @@ def build_RDS():
     x1 = np.random.randint(2, size=(100, 100))  # 500, 500
     x2 = np.copy(x1)
 
-    quadrant = np.random.randint(1,5)
-    if quadrant == 1: # bottom
+    quadrant = np.random.randint(0,4)
+    if quadrant == 0: # bottom
         x2[10:40,35:65] = x1[10:40,36:66] # x2[100:200,200:300] = x1[100:200,201:301] 
-    elif quadrant == 2: # left
+    elif quadrant == 1: # left
         x2[35:65,10:40] = x1[35:65,11:41] # x2[200:300,100:200] = x1[200:300,101:201]
-    elif quadrant == 3: # top
+    elif quadrant == 2: # top
         x2[60:90,35:65] = x1[60:90,36:66] # x2[300:400,200:300] = x1[300:400,201:301]
     else: # right
         x2[35:65,60:90] = x1[35:65,61:91] # x2[200:300,300:400] = x1[200:300,301:401]
